8f1c4988c805a43c1054a159044dcc944fc8a7d2
Neil <ncagabriel02@gmail.com>
2025-04-20 10:04:10 +0800

    hotfix

--- frontend/src/pages/EditTodo.jsx
+++ frontend/src/pages/EditTodo.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
-import "../../public/EditTodo.css";
+import "../assets/EditTodo.css";
 const EditTodo = ({ isOpen, onClose, todo, onSave }) => {
   const [description, setDescription] = useState("");
 
